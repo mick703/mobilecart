20fec40524366e6abee1b95e231ecc79ee9e800b
Ming Yan <mingyan214@gmail.com>
2014-06-25 22:52:56 +1000

    removes the default template, trying out the new side menu and the list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,44 @@
-angular.module('starter.controllers', [])
+angular.module('mobilecart.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('CategoryCtrl', function($scope){
+  $scope.categories = [
+    {title: 'TV & Video'},
+    {title: 'Audio & MP3'},
+    {title: 'Photography'},
+    {title: 'Computers & Tablets'},
+    {title: 'Printers & Office'},
+    {title: 'Mobiles & Wireless'},
+    {title: 'Navigation & Travel'},
+    {title: 'Gaming'},
+    {title: 'Home & Security'},
+    {title: 'Promotions'}
+  ];
 })
+.controller('NavCtrl', function($scope, $ionicSideMenuDelegate){
+  $scope.toggleRightSideMenu = function(){
+    $ionicSideMenuDelegate.toggleRight();
+  }
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
 })
+;
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
 
-.controller('AccountCtrl', function($scope) {
-});
+// .controller('DashCtrl', function($scope) {
+// })
+
+// .controller('FriendsCtrl', function($scope, Friends) {
+//   $scope.friends = Friends.all();
+// })
+
+// .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//   $scope.friend = Friends.get($stateParams.friendId);
+// })
+
+// .controller('AccountCtrl', function($scope) {
+// })
+
+// .controller('ContentController', function($scope, $ionicSideMenuDelegate){
+//   $scope.toggleLeft = function(){
+//     $ionicSideMenuDelegate.toggleLeft();
+//   }
+// });
